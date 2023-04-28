5f31129585e408932c500797a3a7a01fb2c765a1
gabrielalimact <lgcl@ic.ufal.br>
2023-04-28 11:13:50 -0300

    feat: add API usage functions

---
+++ weatherApp/src/api/auth.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'http://localhost:3000/results',
+});
+
+export default instance;
+
+export const getResults = async () => {
+  const { data } = await instance.get('http://localhost:3000/results');
+  return data;
+}
+
+export const getNextDays = async () => {
+  const { data } = await instance.get('http://localhost:3000/results');
+  return data.forecast;
+}
+
+export const getToday = async () => {
+  const { data } = await instance.get('http://localhost:3000/results');
+  return data.forest[0];
+}
+
+export const getTemp = async () => {
+  const { data } = await instance.get('http://localhost:3000/results');
+  return data.temp;
+}
+
+export const getNight = async () => {
+  const { data } = await instance.get('http://localhost:3000/results');
+  return data.current === 'night';
+}
