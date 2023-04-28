9892833a3715275a4431244845aea8c6fd04bce7
gabrielalimact <lgcl@ic.ufal.br>
2023-04-28 12:25:55 -0300

    feat: app general page

--- weatherApp/App.tsx
+++ weatherApp/App.tsx
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, ScrollView } from 'react-native';
+import Home from './src/views/Home';
+import { LinearGradient } from 'expo-linear-gradient';
+import { getResults, getNight } from './src/api/auth';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [isNight, setIsNight] = useState(false);
+
+  useEffect(() => {
+    getNight().then((res) => {
+      setIsNight(res);
+    })
+  }, [isNight]);
+
+  const colors = isNight ? ["#08244F", "#134CB5", "#0B42AB"] : ["#29B2DD", "#33AADD", "#2DC8EA"]
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LinearGradient style={styles.container} colors={colors}>
+      <ScrollView>
+        <StatusBar style="light" />
+        <Home />
+      </ScrollView>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: '100%',
   },
 });
